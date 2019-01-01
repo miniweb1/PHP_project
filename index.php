ada8810607dbf11b6a7584c3b7efa1aa955cdce6
miniweb1 <miniweb@abv.bg>
2019-01-01 21:22:14 +0200

    Remove seconds

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ function clockHandAngle($time) {
     $time = explode(':', $time);
     $hours = $time[0];
     $minutes = $time[1];
-    $seconds = $time[2];
     $degree = 0.5 * (60 * $hours - 11 * $minutes);
     $degree = abs($degree);
     if($degree > 180) {
