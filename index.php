b9afc0682d5e811b4d00f9c6707db38dce792971
miniweb1 <miniweb@abv.bg>
2019-01-03 18:55:59 +0200

    Make some changes

--- index.php
+++ index.php
@@ -4,9 +4,7 @@ function clockHandAngle($time) {
     $hours = $time[0];
     $minutes = $time[1];
     $seconds = $time[2];
-    $minutes += $seconds / 60;
-    $angle = 0.5 * (60 * $hours - 11 * $minutes);
-    $angle = abs($angle);
+    $angle = abs(30 * $hours + 0.5 * $minutes + ($seconds / 120) - (6 * $minutes + 0.1 * $seconds));
     return min($angle, 360 - $angle);
 }
 
@@ -14,7 +12,23 @@ function clockHandAngle2($angle, $timeNow) {
     $timeNow = explode(':', $timeNow);
     $hours = $timeNow[0];
     $minutes = $timeNow[1];
-
+    $seconds = $timeNow[2];
+    $e = 0.09;
+    while (clockHandAngle($hours . ':' . $minutes . ':' . $seconds) - $angle > $e) {
+        $seconds += 1;
+        if($seconds >= 60) {
+            $seconds = 0;
+            $minutes += 1;
+        }
+        if($minutes >= 60) {
+            $minutes = 0;
+            $hours += 1;
+        }
+        if($hours > 12) {
+            $hours -= 12;
+        }
+    }
+    return $hours . ':' . $minutes . ':' . $seconds;
 }
 
-echo clockHandAngle('12:44:33');
+echo clockHandAngle2(0, '12:00:01');
