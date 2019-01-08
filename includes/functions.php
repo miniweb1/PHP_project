09fa04ea815eff58a21d29a8457a3e1b4da57b0e
maggietodorova <how_its_made@abv.bg>
2019-01-08 20:50:51 +0200

    Final changes

--- includes/functions.php
+++ includes/functions.php
@@ -1,17 +1,22 @@
 <?php
 function clockHandAngle($time) {
+    // Get the hours, minutes and seconds from the string
     $time = explode(':', $time);
     $hours = $time[0];
+    $minutes = $time[1];
+    $seconds = $time[2];
+    // Check if hours are greater than 12, so the function can work with 24-hour clock
     if($hours > 12) {
         $hours -= 12;
     }
-    $minutes = $time[1];
-    $seconds = $time[2];
+    // Formula for finding angle
     $angle = abs(30 * $hours + 0.5 * $minutes + ($seconds / 120) - (6 * $minutes + 0.1 * $seconds));
+    // Return the smaller angle
     return min($angle, abs(360 - $angle));
 }
 
 function clockHandAngle2($angle, $timeNow) {
+    // Get the hours, minutes and seconds from the string
     $timeNow = explode(':', $timeNow);
     $hours = $timeNow[0];
     $minutes = $timeNow[1];
@@ -27,11 +32,13 @@ function clockHandAngle2($angle, $timeNow) {
             $minutes = 0;
             $hours += 1;
         }
+        // Check if hours are greater than 12, so the function can work with 24-hour clock
         if($hours > 12) {
             $hours -= 12;
         }
     }
+    // Format the time
     $time = strtotime($hours . ':' . $minutes . ':' . $seconds);
 
-    return  date('H:i:s', $time);
+    return date('H:i:s', $time);
 }
